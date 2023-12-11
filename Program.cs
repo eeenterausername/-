f702f67843c8405ae3ea0b7240552735f039f544
Yervand <baghdasaryanervand@gmail.com>
2023-12-11 23:26:52 +0400

    first commit

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿
+static string[] FilterShortStrings(string[] arr)
+    {
+        int count = 0;
+
+        // Подсчитываем количество строк длиной не более 3 символов
+        for (int i = 0; i < arr.Length; i++)
+        {
+            if (arr[i].Length <= 3)
+            {
+                count++;
+            }
+        }
+        
+        string[] result = new string[count];
+        int index = 0;
+        for (int i = 0; i < arr.Length; i++)
+        {
+            if (arr[i].Length <= 3)
+            {
+                result[index] = arr[i];
+                index++;
+            }
+        }
+
+        return result;
+    }
