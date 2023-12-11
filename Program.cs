abb8191a9d9b6d63b1b6c0775a063bb8505ec580
Yervand <baghdasaryanervand@gmail.com>
2023-12-11 23:39:55 +0400

    Added functions for input and output

--- Program.cs
+++ Program.cs
@@ -1,4 +1,6 @@
-﻿
+﻿using System;
+class Program
+{
 static string[] FilterShortStrings(string[] arr)
     {
         int count = 0;
@@ -11,7 +13,7 @@ static string[] FilterShortStrings(string[] arr)
                 count++;
             }
         }
-        
+
         string[] result = new string[count];
         int index = 0;
         for (int i = 0; i < arr.Length; i++)
@@ -25,3 +27,27 @@ static string[] FilterShortStrings(string[] arr)
 
         return result;
     }
+
+    static void Main(string[] args)
+    {
+        Console.Write("Введите количество строк в массиве: ");
+        int n = int.Parse(Console.ReadLine());
+
+        string[] strings = new string[n];
+        for (int i = 0; i < n; i++)
+        {
+            Console.Write($"Введите строку {i + 1}: ");
+            strings[i] = Console.ReadLine();
+        }
+
+        string[] filteredStrings = FilterShortStrings(strings);
+
+        Console.WriteLine("Строки длиной не более 3 символов:");
+        foreach (string str in filteredStrings)
+        {
+            Console.WriteLine(str);
+        }
+    }
+
+}
+
